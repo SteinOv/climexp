1b7c2390d189ce58bfdd899a5074bb934b81c409
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2019-06-12 12:17:55 +0200

    Detabbed.

--- example_download_script_eobs.sh
+++ example_download_script_eobs.sh
@@ -5,20 +5,20 @@ vars="rr tx tg tn"
 intertype=nearest # "nearest" grid point, or "interpolate".
 standardunits="" # "" or "standardunits"
 if [ $email = insert_your_Climate_Explorer_email_address_here ]; then
-	echo "Please modify this script by inserting your Climate Explorer id at the top of the script"
-	exit -1
+    echo "Please modify this script by inserting your Climate Explorer id at the top of the script"
+    exit -1
 fi
 case $intertype in
-	nearest) in="n";;
-	interpolate) in="i";;
-	*) echo "$0: error: option intertype should be nearest or interpolate, not $intertype"; exit -1;;
+    nearest) in="n";;
+    interpolate) in="i";;
+    *) echo "$0: error: option intertype should be nearest or interpolate, not $intertype"; exit -1;;
 esac
 case $standardunits in
-	"") su="";;
-	standardunits) su="_su";;
-	*) echo "$0: error: option intertype should be nearest or interpolate, not $intertype"; exit -1;;
+    "") su="";;
+    standardunits) su="_su";;
+    *) echo "$0: error: option intertype should be nearest or interpolate, not $intertype"; exit -1;;
 esac
-	
+    
 for var in $vars
 do
     lat=something
@@ -31,21 +31,21 @@ do
             exit
         fi
         field=ensembles_05_${var}_mo
-		file=i${field}_${lon}E_${lat}N_${in}${su}.dat
+        file=i${field}_${lon}E_${lat}N_${in}${su}.dat
         if [ ! -s $file ]; then
-    		curl http://climexp.knmi.nl/get_index.cgi\?id=$email\&field=$field\&lat1=$lat\&lon1=$lon\&intertype=$intertype\&standardunits=$standardunits
-	    	wget http://climexp.knmi.nl/data/$file
-		    if [ ! -s $file ]; then
-			    echo "something went wrong downloading $file"
-			    exit
-			else
-			    c=`file $file | fgrep -c HTML`
-			    if [ $c != 0 ]; then
-			       echo "something went wrong downloading $file"
+            curl http://climexp.knmi.nl/get_index.cgi\?id=$email\&field=$field\&lat1=$lat\&lon1=$lon\&intertype=$intertype\&standardunits=$standardunits
+            wget http://climexp.knmi.nl/data/$file
+            if [ ! -s $file ]; then
+                echo "something went wrong downloading $file"
+                exit
+            else
+                c=`file $file | fgrep -c HTML`
+                if [ $c != 0 ]; then
+                   echo "something went wrong downloading $file"
                     rm $file
-			        exit
-			    fi  
-		    fi
-		fi
-	done
+                    exit
+                fi  
+            fi
+        fi
+    done
 done
